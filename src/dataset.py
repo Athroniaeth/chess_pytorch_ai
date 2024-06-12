9174f31b83a98fc6060d56a9f2647ad4c891ed39
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-06-12 02:41:35 +0200

    Separates preprocessing and dataset roles & Polars memory optimization - Create 'split_dataset' for create preprocess dataframe so you don't have to redo the calculation every time you train - Remove clip preprocess step because useless - Change typing on map_elements for remove warning and optimization memory - ChessDataset now use preprocess dataframe for give tensor PyTorch - Add to ChessDataset normalisation methods and hist_score (plt)

--- src/dataset.py
+++ src/dataset.py
@@ -1,61 +1,71 @@
+import logging
+
+import numpy as np
 import polars
 import torch
 from matplotlib import pyplot as plt
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, SubsetRandomSampler, DataLoader
 
 from src.tokenizer import fen_to_array
+from src.utils import logging_time
 
 path = r"C:\Users\pierr\PycharmProjects\chess_pytorch_ai\data\fen_to_stockfish_evaluation.csv"
 
 
-class FenScoreDataset(Dataset):
+@logging_time('preprocess_dataframe')
+def preprocess_dataframe(
+        dataframe: polars.DataFrame,
+        fen_column: str = "column_1",
+        score_column: str = "column_2",
+        max_length: int = 100_000,
+):
     """
-    Dataset contenant les FEN et les scores d'évaluation de Stockfish.
+    Prétraite le DataFrame pour le transformer en un format utilisable par le modèle.
 
     Args:
-        dataframe (polars.DataFrame): DataFrame contenant les données.
+        dataframe (polars.DataFrame): DataFrame à prétraiter.
         fen_column (str): Nom de la colonne contenant les FEN. Defaults to "column_1".
         score_column (str): Nom de la colonne contenant les scores. Defaults to "column_2".
-        dtype (torch.dtype): Type de données des tenseurs. Defaults to torch.float32.
-        max_length (int): Taille maximale du dataset
+        max_length (int): Taille maximale du DataFrame. Defaults to 100_000.
 
     Notes:
         - La colonne 'score' contient des espaces (erreur du créateur?), ils sont supprimés.
     """
+    # Supprime les lignes non désirées
+    dataframe = dataframe.slice(0, max_length)
 
-    def __init__(
-            self,
-            dataframe: polars.DataFrame,
-            fen_column: str = "column_1",
-            score_column: str = "column_2",
+    # Convertit les FEN en tableau numpy / # MapWithoutReturnDtypeWarning
+    expression = polars.col(fen_column).map_elements(fen_to_array, return_dtype=polars.List(polars.UInt8))
+    dataframe = dataframe.with_columns(expression)
 
-            dtype: torch.dtype = torch.float32,
-            max_length: int = 10_000,
-            min_score: int = -20,
-            max_score: int = 20
-    ):
-        # Supprime les lignes non désirées
-        dataframe = dataframe.slice(0, max_length)
+    # Convertit le dtype string de 'score' en dtype float
+    expression = polars.col(score_column).str.replace(" ", "").cast(polars.Float32)
 
-        # Convertit les FEN en tableau numpy
-        expression = polars.col(fen_column).map_elements(fen_to_array)
-        dataframe = dataframe.with_columns(expression)
+    # Convertit le dtype string de 'score' en dtype float
+    dataframe = dataframe.with_columns(expression)
 
-        # Convertit le dtype string de 'score' en dtype float
-        expression = polars.col(score_column).str.replace(" ", "").cast(polars.Float32)
+    # Renomme les colonnes en 'fen' et 'score'
+    dataframe = dataframe.rename({fen_column: 'fen', score_column: 'score'})
 
-        # Convertit le dtype string de 'score' en dtype float
-        dataframe = dataframe.with_columns(expression)
+    return dataframe
 
-        # Remplace les valeurs "aberrantes" pour la normalisation
-        expression = polars.col(score_column).clip(min_score, max_score)
-        dataframe = dataframe.with_columns(expression)
 
-        # Renomme les colonnes en 'fen' et 'score'
-        dataframe = dataframe.rename({fen_column: 'fen', score_column: 'score'})
+class ChessDataset(Dataset):
+    """
+    Dataset contenant les FEN et les scores d'évaluation de Stockfish.
 
-        self.dataframe = dataframe
-        self.dtype = dtype
+    Args:
+        preprocess_df (polars.DataFrame): DataFrame contenant les données.
+    """
+
+    def __init__(self, preprocess_df: polars.DataFrame):
+        self.min_val = preprocess_df['score'].min()
+        self.max_val = preprocess_df['score'].max()
+
+        expression = (polars.col('score') - self.min_val) / (self.max_val - self.min_val)
+        preprocess_df = preprocess_df.with_columns(expression)
+
+        self.dataframe = preprocess_df
 
     def __getitem__(self, index: int):
         """
@@ -73,13 +83,19 @@ class FenScoreDataset(Dataset):
         score = self.dataframe['score'][index]
 
         # Convertit le tableau numpy en tenseur PyTorch
-        tensor_array = torch.tensor(array, dtype=self.dtype)
-        tensor_score = torch.tensor(score, dtype=self.dtype)
+        tensor_array = torch.tensor(array, dtype=torch.float32)
+        tensor_score = torch.tensor(score, dtype=torch.float32)
 
         return tensor_array, tensor_score
 
     def __len__(self):
         return len(self.dataframe)
+
+    def normalize_score(self, score: float) -> float:
+        return (score - self.min_val) / (self.max_val - self.min_val)
+
+    def denormalize_score(self, score: float) -> float:
+        return score * (self.max_val - self.min_val) + self.min_val
 
     def hist_score(self, bins: int = 51):
         """
@@ -110,6 +126,63 @@ class FenScoreDataset(Dataset):
         plt.show()
 
 
-dataset = polars.read_csv(path, has_header=False)
-dataset = FenScoreDataset(dataset)
-dataset.hist_score()
+def split_dataset(
+        dataset: Dataset,
+
+        batch_size: int = 256,
+        shuffle_dataset: bool = True,
+        random_seed: int = 42,
+
+        ratio_tests: float = 0.2,
+        ratio_validation: float = 0.2
+):
+    """
+    Sépare le dataset en trois parties : entraînement, tests et validation.
+
+    Args:
+        dataset (Dataset): Dataset PyTorch à splitter.
+        batch_size (int): Taille des batchs. Defaults to 32.
+        shuffle_dataset (bool): Mélange le dataset. Defaults to True.
+        random_seed (int): Graine aléatoire pour le mélange. Defaults to 42.
+        ratio_tests (float): Ratio du dataset pour les tests. Defaults to 0.1.
+        ratio_validation (float): Ratio du dataset pour la validation. Defaults to 0.1.
+
+    Returns:
+        Tuple[DataLoader, DataLoader, DataLoader]: Tuple contenant les DataLoaders pour l'entraînement, les tests et la validation.
+    """
+    ratio = round(1 - ratio_tests - ratio_validation, 2)
+    logging.info(f"Splitting dataset, {ratio=}, {ratio_tests=}, {ratio_validation=}")
+
+    # Prépare une liste d'indices du dataset
+    dataset_size = len(dataset)  # noqa
+    indices = [i for i in range(dataset_size)]
+
+    # Calcule les slices pour chaque partie du dataset
+    split_tests = dataset_size // (1 / ratio_tests)
+    split_validation = dataset_size // (1 / ratio_validation)
+    split_tests, split_validation = int(split_tests), int(split_validation)
+
+    slice_train = slice(split_tests + split_validation, None)
+    slice_tests = slice(split_tests, split_tests + split_validation)
+    slice_validation = slice(None, split_tests)
+
+    if shuffle_dataset:
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+
+    # Applique les slices aux indices
+    train_indices = indices[slice_train]
+    tests_indices = indices[slice_tests]
+    validation_indices = indices[slice_validation]
+
+    # Crée les samplers (sous-ensembles du dataset)
+    train_sampler = SubsetRandomSampler(train_indices)
+    tests_sampler = SubsetRandomSampler(tests_indices)
+    validation_sampler = SubsetRandomSampler(validation_indices)
+
+    # Crée les DataLoaders
+    train_loader = DataLoader(dataset, batch_size=batch_size, sampler=train_sampler)
+    tests_loader = DataLoader(dataset, batch_size=batch_size, sampler=tests_sampler)
+    validation_loader = DataLoader(dataset, batch_size=batch_size, sampler=validation_sampler)
+
+    return train_loader, tests_loader, validation_loader
